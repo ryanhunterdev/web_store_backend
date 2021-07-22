a6f4ff0d74b9542ed4e8f9c1e4d9f4251ec446f2
ryanhunterdev <81719986+ryanhunterdev@users.noreply.github.com>
2021-07-22 16:23:31 +1000

    build

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
-
 app.use("/api", getRoutes());
 
-app.get("*", function (req, res) {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path')
+  app.use(express.static(path.join(__dirname, 'build')));
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
